4a4cebf833318f172a976d4308b134572bc3287e
rubywang130701 <109776578+rubywang130701@users.noreply.github.com>
2023-02-28 19:49:10 -0500

    Add files via upload

---
+++ react-library-Front-end-16/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+
+export const App = () => {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </div>
+  );
+};
---
+++ react-library-Front-end-16/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -0,0 +1,200 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+import ReviewModel from "../../models/ReviewModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { StarsReview } from "../Utils/StarsReview";
+import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
+import { LatestReviews } from "./LatestReviews";
+
+import ReviewRequestModel from "../../models/ReviewRequestModel";
+
+export const BookCheckoutPage = () => {
+
+
+    const [book, setBook] = useState<BookModel>();
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    // Review State
+    const [reviews, setReviews] = useState<ReviewModel[]>([])
+    const [totalStars, setTotalStars] = useState(0);
+    const [isLoadingReview, setIsLoadingReview] = useState(true);
+
+    const [isReviewLeft, setIsReviewLeft] = useState(false);
+    const [isLoadingUserReview, setIsLoadingUserReview] = useState(true);
+
+    // Loans Count State
+    const [currentLoansCount, setCurrentLoansCount] = useState(0);
+    const [isLoadingCurrentLoansCount, setIsLoadingCurrentLoansCount] = useState(true);
+
+    // Is Book Check Out?
+    const [isCheckedOut, setIsCheckedOut] = useState(false);
+    const [isLoadingBookCheckedOut, setIsLoadingBookCheckedOut] = useState(true);
+
+    const bookId = (window.location.pathname).split('/')[2];
+
+    useEffect(() => {
+        const fetchBook = async () => {
+            const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+            const response = await fetch(baseUrl);
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseJson = await response.json();
+
+            const loadedBook: BookModel = {
+                id: responseJson.id,
+                title: responseJson.title,
+                author: responseJson.author,
+                description: responseJson.description,
+                copies: responseJson.copies,
+                copiesAvailable: responseJson.copiesAvailable,
+                category: responseJson.category,
+                img: responseJson.img,
+            };
+
+            setBook(loadedBook);
+            setIsLoading(false);
+        };
+        fetchBook().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, [isCheckedOut]);
+
+    useEffect(() => {
+        const fetchBookReviews = async () => {
+            const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}`;
+
+            const responseReviews = await fetch(reviewUrl);
+
+            if (!responseReviews.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseJsonReviews = await responseReviews.json();
+
+            const responseData = responseJsonReviews._embedded.reviews;
+
+            const loadedReviews: ReviewModel[] = [];
+
+            let weightedStarReviews: number = 0;
+
+            for (const key in responseData) {
+                loadedReviews.push({
+                    id: responseData[key].id,
+                    userEmail: responseData[key].userEmail,
+                    date: responseData[key].date,
+                    rating: responseData[key].rating,
+                    book_id: responseData[key].bookId,
+                    reviewDescription: responseData[key].reviewDescription,
+                });
+                weightedStarReviews = weightedStarReviews + responseData[key].rating;
+            }
+
+            if (loadedReviews) {
+                const round = (Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2).toFixed(1);
+                setTotalStars(Number(round));
+            }
+
+            setReviews(loadedReviews);
+            setIsLoadingReview(false);
+        };
+
+        fetchBookReviews().catch((error: any) => {
+            setIsLoadingReview(false);
+            setHttpError(error.message);
+        })
+    }, [isReviewLeft]);
+
+  
+    if (isLoading || isLoadingReview || isLoadingCurrentLoansCount || isLoadingBookCheckedOut || isLoadingUserReview) {
+        return (
+            <SpinnerLoading />
+        )
+    }
+
+    if (httpError) {
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+    async function checkoutBook() {
+        const url = `http://localhost:8080/api/books/secure/checkout/?bookId=${book?.id}`;
+       
+        const checkoutResponse = await fetch(url);
+        if (!checkoutResponse.ok) {
+            throw new Error('Something went wrong!');
+        }
+        setIsCheckedOut(true);
+    }
+
+    async function submitReview(starInput: number, reviewDescription: string) {
+        let bookId: number = 0;
+        if (book?.id) {
+            bookId = book.id;
+        }
+
+        const reviewRequestModel = new ReviewRequestModel(starInput, bookId, reviewDescription);
+        const url = `http://localhost:8080/api/reviews/secure`;
+       
+        const returnResponse = await fetch(url);
+        if (!returnResponse.ok) {
+            throw new Error('Something went wrong!');
+        }
+        setIsReviewLeft(true);
+    }
+
+    return (
+        <div>
+            <div className='container d-none d-lg-block'>
+                <div className='row mt-5'>
+                    <div className='col-sm-2 col-md-2'>
+                        {book?.img ?
+                            <img src={book?.img} width='226' height='349' alt='Book' />
+                            :
+                            <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226'
+                                height='349' alt='Book' />
+                        }
+                    </div>
+                    <div className='col-4 col-md-4 container'>
+                        <div className='ml-2'>
+                            <h2>{book?.title}</h2>
+                            <h5 className='text-primary'>{book?.author}</h5>
+                            <p className='lead'>{book?.description}</p>
+                            <StarsReview rating={totalStars} size={32} />
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
+            </div>
+            <div className='container d-lg-none mt-5'>
+                <div className='d-flex justify-content-center alighn-items-center'>
+                    {book?.img ?
+                        <img src={book?.img} width='226' height='349' alt='Book' />
+                        :
+                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226'
+                            height='349' alt='Book' />
+                    }
+                </div>
+                <div className='mt-4'>
+                    <div className='ml-2'>
+                        <h2>{book?.title}</h2>
+                        <h5 className='text-primary'>{book?.author}</h5>
+                        <p className='lead'>{book?.description}</p>
+                        <StarsReview rating={totalStars} size={32} />
+                    </div>
+                </div>
+                <hr />
+                <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
+            </div>
+        </div>
+    );
+}
